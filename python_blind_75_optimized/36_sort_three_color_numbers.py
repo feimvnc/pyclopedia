903e835a6fa777aad504db5d7e0fb08869d839ab
feimvnc <feimvnc@gmail.com>
2023-08-27 08:56:45 +0100

    add in-place methodd

--- python_blind_75_optimized/36_sort_three_color_numbers.py
+++ python_blind_75_optimized/36_sort_three_color_numbers.py
@@ -1,3 +1,6 @@
+""" 
+Sort color number in place efficiently, don't use built-in sort() function.
+"""
 from typing import List 
 
 class Solution:
@@ -19,5 +22,29 @@ class Solution:
                 ptr += 1 
         print(nums)
 
+    def sortColorNumbers_2(self, nums: List[int]) -> None:
+        """ Do not return anything, modify nums in-place """
+        l, r = 0, len(nums) - 1 
+        i = 0 
+
+        def swap(i,j):
+            tmp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = tmp 
+        
+        while i <= r:
+            if nums[i] == 0:
+                swap(l, i)
+                l += 1 
+            elif nums[i] == 2:
+                swap(i, r)
+                r -= 1 
+                i -= 1 
+            i += 1 
+        print(nums)
+
+
 nums = [2,1,0,2,1,0,1]
-Solution().sortColorNumbers(nums)
+Solution().sortColorNumbers(nums) 
+Solution().sortColorNumbers_2(nums) 
+
